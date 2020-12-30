5791d02b56fcf5364a3982ed0d05917683bb71f2
Lin Wai Phyo <linwaiphyo.akp@gmail.com>
2020-12-30 08:36:28 +0630

    bgcolour and buttons done

--- src/lwp/MenuPage.java
+++ src/lwp/MenuPage.java
@@ -5,6 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
 public class MenuPage extends JFrame {
 	private JPanel contentPane;
@@ -29,10 +33,36 @@ public class MenuPage extends JFrame {
 	 * Create the frame.
 	 */
 	public MenuPage() {
+		getContentPane().setBackground(Color.CYAN);
 		contentPane = new JPanel();
 		setBounds(100, 100, 1257, 771);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		getContentPane().setLayout(null);
+		
+		JButton homeButton = new JButton("Home");
+		homeButton.setForeground(Color.WHITE);
+		homeButton.setBackground(Color.BLACK);
+		homeButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		homeButton.setFont(new Font("Tekton Pro", Font.PLAIN, 18));
+		homeButton.setBounds(96, 216, 102, 34);
+		getContentPane().add(homeButton);
+		
+		JButton registerButton = new JButton("Register");
+		registerButton.setForeground(Color.WHITE);
+		registerButton.setBackground(Color.BLACK);
+		registerButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		registerButton.setFont(new Font("Tekton Pro", Font.PLAIN, 18));
+		registerButton.setBounds(96, 306, 102, 34);
+		getContentPane().add(registerButton);
+		
+		JButton btnNewButton = new JButton("Display");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.setFont(new Font("Tekton Pro", Font.PLAIN, 18));
+		btnNewButton.setVerticalAlignment(SwingConstants.BOTTOM);
+		btnNewButton.setBounds(96, 392, 102, 34);
+		getContentPane().add(btnNewButton);
 		contentPane.setBorder(new EmptyBorder(5,5,5,5));
 		contentPane.setLayout(null);
 		
